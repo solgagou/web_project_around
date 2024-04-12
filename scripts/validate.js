f5b0ffca6f9abcb8e210f5851cb1f57f3e3908e0
solgagou <solgagou@gmail.com>
2024-04-12 18:54:58 -0300

    fix: change validate.js code on functions

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,80 +1,55 @@
 
   const formElement = document.querySelector(".form");
-  const formInput = formElement.querySelector(".form__input");
-  const formError = formElement.querySelector(`.${formInput.id}-error`);
+  const inputElement = formElement.querySelector(".form__input");
+  
 
 
   
-  const showInputError = (inputElement, errorMessage) => {
-    element.classList.add("form__input-error");
-    // Muestra el mensaje de error
+  const showInputError = (formElement, inputElement, errorMessage) => {
+    const formError = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add("form__input-error");
     formError.textContent = errorMessage;
     formError.classList.add("form__input-error_active");
   
   };
   
-  const hideInputError = (element) => {
-    element.classList.remove("form__input-error");
-    // Oculta el mensaje de error
+  const hideInputError = (formElement, inputElement) => {
+    const formError = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove("form__input-error");
     formError.classList.remove("form__input-error_active");
-     // Restablece el error
     formError.textContent = "";
 
   };
 
-  const isValid = () => {
-    if (!formInput.validity.valid) {
-      // Si NO lo es (!), muestra el elemento erróneo// El propio mensaje de error es el segundo parámetro de la función
-      showInputError(formInput, formInput.validationMessage);
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
     } else {
-      // Si es válido, oculta el elemento erróneo
-      hideInputError(formInput);
+        hideInputError(formElementf, inputElement);
     }
   };
   
-  formElement.addEventListener("submit", function (evt) {
-    // Cancela la acción del navegador por defecto, de modo que al hacer clic en el botón "Enviar" no se actualice la página
-    evt.preventDefault();
-  });
   
-  // Llama a la función isValid() para cada entrada de caracteres
-  formInput.addEventListener("Input se ejecuta", isValid);
-  
-  const setEventListeners = (formElement) => {
-    // Encuentra todos los campos dentro del formulario y
-    // crea un array a partir de estos, utilizando el método Array.from()
+  const setEventListeners = (formElement, inputElement) => {
     const inputList = Array.from(formElement.querySelectorAll(".form__input"));
-  
-    // Itera sobre el array obtenido
-    inputList.forEach((inputElement) => {
-      // agrega el controlador de eventos de entrada a cada campo
-      inputElement.addEventListener("input", () => {
-        // Llama a la función isValid() dentro del callback
-        // y pásale el formulario y el elemento a comprobar
-        isValid(inputElement)
+      inputList.forEach((inputElement) => {
+        inputElement.addEventListener("input", function () {
+        checkInputValidity(formElement, inputElement);
       });
     });
   };
 
   const enableValidation = () => {
-    // Encontrará todos los formularios con la clase especificada en el DOM y
-    // creará un array, a partir de estos, utilizando el método Array.from()
     const formList = Array.from(document.querySelectorAll(".form"));
-  
-    // Itera sobre el array obtenido
     formList.forEach((formElement) => {
       formElement.addEventListener("submit", (evt) => {
-        // Cancela el comportamiento por defecto de cada formulario
         evt.preventDefault();
       });
   
-      // Llama a la función setEventListeners() para cada formulario
-      // tomando un elemento del formulario como argumento
       setEventListeners(formElement);
     });
   };
   
-  // Llama a la función
   enableValidation();
   
   enableValidation({
